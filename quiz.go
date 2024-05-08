1d1909e2b1b445a2e28a9670ad5227da852b641d
PATRICK HESSION <G00394619@atu.ie>
2024-05-08 22:17:58 +0100

    Refactor quiz.go to fix case-insensitive validation for RTX 3090 and Ryzen 9 3900x cores

--- quiz.go
+++ quiz.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to the quiz!")
 	fmt.Println("What is your name?")
-	var name string 
+	var name string
 	fmt.Scan(&name)
 	fmt.Printf("Hello, %v, let's get started! \n", name)
 
@@ -13,15 +13,14 @@ func main() {
 	var age uint
 	fmt.Scan(&age)
 
-
 	if age >= 18 {
 		fmt.Println("You are old enough to take the quiz!")
 	} else {
 		fmt.Println("You are not old enough to take the quiz!")
 		return
 	}
-  score := 0
-  num_Questions := 2
+	score := 0
+	num_Questions := 2
 
 	fmt.Printf("What is better the RTX 3080 or the RTX 3090? ")
 	var answer string
@@ -29,11 +28,10 @@ func main() {
 	fmt.Scan(&answer, &answer2)
 	fmt.Printf("You answered: %v %v \n", answer, answer2)
 
-
-	if answer + " " + answer2 == "RTX 3090" || answer + " " + answer2 == "rtx 3090"|| answer + " " + answer2 == "rtX 3090"{
+	if answer+" "+answer2 == "RTX 3090" || answer+" "+answer2 == "rtx 3090" || answer+" "+answer2 == "rtX 3090" {
 		fmt.Println("Correct!")
-		score ++
-	}else {
+		score++
+	} else {
 		fmt.Println("Incorrect!")
 	}
 
@@ -43,7 +41,7 @@ func main() {
 
 	if coresAnswer == 12 {
 		fmt.Println("Correct!")
-		score ++
+		score++
 	} else {
 		fmt.Println("Incorrect!")
 	}
