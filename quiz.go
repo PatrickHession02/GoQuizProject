daddac17942ea852bfb3bac66a9a14c2d948ccee
PATRICK HESSION <G00394619@gmit.ie>
2024-05-08 20:58:04 +0100

    Refactor name variable initialization and print statement, and add age input in quiz.go

--- quiz.go
+++ quiz.go
@@ -7,5 +7,10 @@ func main() {
 	fmt.Println("What is your name?")
 	var name string 
 	fmt.Scan(&name)
-	fmt.Printf("Hello, %v, let's get started!", name)
+	fmt.Printf("Hello, %v, let's get started! \n", name)
+
+	fmt.Printf("Enter your age ")
+	var age uint
+	fmt.Scan(&age)
+	fmt.Println(age >= 18)
 }
