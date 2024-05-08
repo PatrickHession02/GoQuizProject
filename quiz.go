b17919c57d22b8cf8af77c8e29cbc9fc8cfc064b
PATRICK HESSION <G00394619@gmit.ie>
2024-05-08 20:31:26 +0100

    Refactor name variable initialization and print statement in quiz.go

--- quiz.go
+++ quiz.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to the quiz!")
-	name := "Patrick"
-	age := 21
-	fmt.Printf("Hello %s, let's get started!\nYou are %v", name, age)
+	var name string 
+	fmt.Scan(&name)
+
+	fmt.Println("Hello", name, "let's get started!")
 }
